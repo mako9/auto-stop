4641ab29018a7f97cf35d923594a4f8bdb83fed8
Mario Kohlhoff <mario.kohlhoff@edag.com>
2024-02-08 11:33:17 +0100

    Move sleep check to Watch App to avoid permission issue due to locked screen

--- autostop/ContentViewModel.swift
+++ autostop/ContentViewModel.swift
@@ -9,73 +9,41 @@ import Foundation
 import SwiftUI
 
 class ContentViewModel: ObservableObject {
-    private let healthKitManager = HealthKitManager()
     private let audioManager = AudioManager()
     private let watchSyncService = WatchSyncService()
-    private let dateFormatter: DateFormatter
 
     @Published var stopDateString: String?
     @Published var timerStarted: Bool = false
-    @Published var isRunning: Bool = false
-
-    private var checkTimer: Timer?
 
     init() {
-        dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yy HH:mm"
-        
         watchSyncService.dataReceived = { (key, message) in
-            switch key {
-            case .watchOSTimerStarted:
-                self.startTimer()
-            case .watchOSTimerStopped:
-                self.stopTimer()
-            default:
-                Logger.shared.verbose("Irrelevant key: \(key.rawValue)")
+            DispatchQueue.main.async {
+                switch key {
+                case .watchOSTimerStarted:
+                    self.timerStarted = true
+                case .watchOSTimerStopped:
+                    self.timerStarted = false
+                case .watchOSSleepDetected:
+                    self.timerStarted = false
+                    self.stopDateString = message as? String
+                    self.stopAudio()
+                default:
+                    Logger.shared.verbose("Irrelevant key: \(key.rawValue)")
+                }
             }
         }
     }
 
     func startTimer() {
-        checkTimer = Timer.scheduledTimer(withTimeInterval: 10, repeats: true, block: { _ in
-            self.checkSleep()
-        })
-        timerStarted = true
         watchSyncService.sendMessage(.iOSTimerStarted, "Timer started")
     }
 
     func stopTimer() {
-        checkTimer?.invalidate()
-        checkTimer = nil
-        DispatchQueue.main.async {
-            self.timerStarted = false
-        }
-        self.watchSyncService.sendMessage(.iOSTimerStopped, "Timer stopped")
+        watchSyncService.sendMessage(.iOSTimerStopped, "Timer stopped")
     }
 
-    private func checkSleep() {
-        if isRunning {
-            Logger.shared.debug("Sleep check already running")
-            return
-        }
-        DispatchQueue.main.async {
-            self.isRunning = true
-        }
-        healthKitManager.isSleeping { [self] shouldStop in
-            DispatchQueue.main.async {
-                self.isRunning = false
-            }
-            guard shouldStop else {
-                return
-            }
-            if self.audioManager.stopAudio() {
-                self.stopTimer()
-                let dateString = self.dateFormatter.string(from: Date())
-                DispatchQueue.main.async {
-                    self.stopDateString = dateString
-                }
-                self.watchSyncService.sendMessage(.iOSSleepDetected, dateString)
-            }
-        }
+    private func stopAudio() {
+        let stoppedAudio = audioManager.stopAudio()
+        Logger.shared.debug("Stopped audio successfully: \(stoppedAudio)")
     }
 }
